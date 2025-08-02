7cc82596e3a72518be05e3bd6acd3423016903e6
Question-04 <alok8official8@gmail.com>
2025-08-02 17:21:41 +0530

    Fix syntax error in categories.tsx - remove extra closing tag

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -204,7 +204,6 @@ const CategoriesPage = () => {
                       ))}
                     </div>
                   )}
-                </a>
               </Link>
             ))}
           </div>
